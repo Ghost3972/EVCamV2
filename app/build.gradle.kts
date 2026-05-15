133e0b367048b402962e427a4b2d4a097f379b2c
user <user@MacBook-Pro.local>
2026-05-15 13:53:15 +0800

    fix: normalize zig libc file newlines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 80
-        versionName = "2.0.0-test-05151348"
+        versionName = "2.0.0-test-05151352"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
